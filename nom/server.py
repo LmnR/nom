f88c742944afd33ec550bc8f0b9740cbf9a552a2
Jin Peng <jin.peng@columbia.edu>
2016-11-17 05:43:11 -0500

    pre-final

--- nom/server.py
+++ nom/server.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+  #!/usr/bin/env python2.7
 
 """
 Columbia W4111 Intro to databases
@@ -13,7 +13,11 @@ Read about it online.
 import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response
+from flask import Flask, request, render_template, g, redirect, Response, url_for
+import time
+import sys
+
+
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
@@ -33,8 +37,9 @@ app = Flask(__name__, template_folder=tmpl_dir)
 #     DATABASEURI = "postgresql://ewu2493:foobar@<IP_OF_POSTGRE_SQL_SERVER>/postgres"
 #
 # Swap out the URI below with the URI for the database created in part 2
-DATABASEURI = "sqlite:///test.db"
-
+DATABASEURI = "postgresql://yt2456:98c5y@104.196.175.120/postgres"
+#104.196.175.120
+#104.196.180.73
 
 #
 # This line creates a database engine that knows how to connect to the URI above
@@ -57,12 +62,12 @@ engine = create_engine(DATABASEURI)
 # 
 # The setup code should be deleted once you switch to using the Part 2 postgresql database
 #
-engine.execute("""DROP TABLE IF EXISTS test;""")
-engine.execute("""CREATE TABLE IF NOT EXISTS test (
-  id serial,
-  name text
-);""")
-engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
+#engine.execute("""DROP TABLE IF EXISTS test;""")
+#engine.execute("""CREATE TABLE IF NOT EXISTS test (
+#  id serial,
+#  name text
+#);""")
+#engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
 #
 # END SQLITE SETUP CODE
 #
@@ -126,7 +131,7 @@ def index():
   #
   # example of a database query
   #
-  cursor = g.conn.execute("SELECT name FROM test")
+  cursor = g.conn.execute("SELECT name FROM restaurant")
   names = []
   for result in cursor:
     names.append(result['name'])  # can also be accessed using result[0]
@@ -180,6 +185,197 @@ def another():
   return render_template("anotherfile.html")
 
 
+@app.route('/allrestaurant')
+def allrestaurant():
+
+  #g.conn.execute("Insert into restaurant values (12, 'Thai Market', '11', '22')")
+
+  cursor = g.conn.execute("""select r.name, rc.cuisince, rc.price, a.street, a.city, a.state, a.zip from restaurant r, restaurantcategory rc, address a where (r.restaurantid = 
+                             rc.restaurantid and r.restaurantid = a.restaurantid);""")
+
+  names = []
+  for result in cursor:
+    name = result[0]
+    cuisine = result[1]
+    address = result[3].strip() + ", " + result[4].strip() + ", " + result[5].strip() + ", " + str(result[6]).strip()
+    names.append([name, cuisine, address])  # can also be accessed using result[0]
+  cursor.close()
+
+
+  context = dict(data = names)
+
+
+  return render_template("allrestaurant.html", **context)
+
+
+@app.route('/addarestaurant')
+def addarestaurant():
+
+  return render_template("addarestaurant.html")
+
+@app.route('/menu/<restaurant>')
+def menu(restaurant):
+  cursor = g.conn.execute(""" select name, price from items i where 
+                              i.restaurantid = (select restaurantid from 
+                              restaurant r where r.name = %s);""", restaurant)
+
+  items = []
+  # items.append("Item" + ' ' * 20 + "Price")
+  for result in cursor:
+      items.append([result['name'],result['price']])
+
+  cursor.close()
+
+  context = dict(data = items, rest = restaurant)
+  return render_template("menu.html", **context)
+
+
+@app.route('/addmenu/<restaurant>', methods = ['POST'])
+def addmenu(restaurant):
+  print("restaurant")
+  name = check(request.form['item'])
+  price = check(request.form['pr'])
+
+  if name == None or price == None:
+    
+    context = "/menu/" + restaurant
+    return render_template("error.html", url = context)
+
+  id = int(g.conn.execute("select last_value from items_id_seq").first()['last_value'])
+
+
+  restid = int(g.conn.execute("""select restaurantid from restaurant where name = %s""", restaurant).first()['restaurantid'])
+  
+  cmd = 'INSERT INTO items values ((:id), (:name1), (:name2), (:name3))'
+  g.conn.execute(text(cmd), id = id, name1 = name, name2 = price, name3 = restid)
+
+  seqname = 'items_id_seq'
+  inc = 'Select nextval((:name))'
+  g.conn.execute(text(inc), name = seqname)
+  
+
+  return redirect(url_for('menu', restaurant = restaurant))
+
+
+
+@app.route('/addrestaurant', methods = ['POST'])
+def addrestaurant():
+
+  rname = check(request.form['nm']) 
+  otime = check(request.form['op'])
+  ctime = check(request.form['ct'])
+  category = check(request.form['cat']) 
+  waiting = check(request.form['wt'])
+  price = check(request.form['pr'])
+
+
+  street = check(request.form['str']) 
+  city = check(request.form['cty'])
+  state = check(request.form['sta'])
+  country = check(request.form['cou']) 
+  z = check(request.form['zip']) 
+
+
+  if rname == None or otime == None or ctime == None or category == None or waiting == None or price == None or street == None or city == None or state == None or country == None or z == None:    
+    
+    context = "/addarestaurant"
+    return render_template("error.html", url = context)
+
+  id = int(g.conn.execute("select last_value from rest_id_seq").first()['last_value'])
+  
+  
+  cmd = 'INSERT INTO restaurant values ((:id), (:name1), (:name2), (:name3))'
+  g.conn.execute(text(cmd), id = id, name1 = rname, name2 = otime, name3 = ctime)
+
+  try:
+    cid = g.conn.execute(" select cid from categorys where cname = %s", category).first()['cid']
+
+    restcat = 'INSERT INTO restaurantcategory values ((:cid), (:cat), (:wait), (:pr))'
+    g.conn.execute(text(restcat), cid = int(cid), cat = category, wait = waiting, pr = price)
+  except TypeError: 
+    newcid = int(g.conn.execute("select last_value from cat_id_seq").first()['last_value'])
+
+   
+
+    cat = 'INSERT INTO categorys values ((:cid), (:cname))'
+
+    g.conn.execute(text(cat), cid = newcid, cname = category)
+
+    restcat = 'INSERT INTO restaurantcategory values ((:cid), (:cat), (:wait), (:pr))'
+    g.conn.execute(text(restcat), cid = newcid, cat = category, wait = waiting, pr = price)
+
+    
+
+    
+    seqname = 'cat_id_seq'
+    inc = 'Select nextval((:name))'
+    g.conn.execute(text(inc), name = seqname)
+  
+
+  
+  
+
+  add = 'INSERT INTO address values ((:id), (:x1), (:x2), (:x3), (:x4), (:x5), (:x6))'
+  g.conn.execute(text(add), id = id, x1 = street, x2 = z, x3 = city, x4 = state, x5 = country, x6 = id)
+
+  seqname = 'rest_id_seq'
+  inc = 'Select nextval((:name))'
+  g.conn.execute(text(inc), name = seqname)
+
+
+  return redirect('/allrestaurant')
+
+def check(statement):
+  
+  if (statement == None or statement == ""):
+    return None
+
+  s = statement.lstrip(' ').lower()
+  
+  if s== None or s[0] == "'" or 'delete' in s or 'insert' in s or s[-1] == '-':
+    return None 
+  else:
+    return statement
+
+@app.route('/checkhistory')
+def checkhistory():
+  return render_template("checkhistory.html")
+
+
+@app.route('/userinfo', methods = ['POST'])
+def userinfo():
+  uname = check(request.form['us'])
+  password = check(request.form['ps'])
+
+  if uname == None or password == None:
+    return render_template("error.html")
+
+  print(uname + " " + password)
+
+  try:
+    uid = g.conn.execute(" select uid from users where email = %s and password = %s", uname, password).first()['uid']
+
+
+    cursor = g.conn.execute("""select r.name, u.timespicked from userhistory u, restaurant r
+                               where (u.restaurantid = r.restaurantid and u.uid = %s);""", int(uid))
+    
+    names = []
+    for result in cursor:
+      print(result)
+      names.append([result[0], str(result[1])])
+
+    cursor.close()
+
+    context = dict(data = names)
+ 
+    return render_template("userhistory.html", **context)
+
+  except TypeError:
+
+    return render_template("error.html")
+
+
+
 # Example of adding new data to the database
 @app.route('/add', methods=['POST'])
 def add():
@@ -187,6 +383,7 @@ def add():
   print (name)
   cmd = 'INSERT INTO test(name) VALUES (:name1), (:name2)';
   g.conn.execute(text(cmd), name1 = name, name2 = name);
+
   return redirect('/')
 
 
@@ -194,6 +391,64 @@ def add():
 def login():
     abort(401)
     this_is_never_executed()
+
+
+@app.route('/search')
+def search():
+
+  cursor = g.conn.execute(""" SELECT DISTINCT cuisince FROM restaurantcategory """)
+  info = []
+  for result in cursor:
+    info.append(result['cuisince'])
+  cursor.close()
+
+  
+  context = dict(info = info)
+  return render_template("search.html", **context)
+
+
+@app.route('/suggest', methods=['POST'])
+def suggest():
+  cuisine = check(request.form['cuisine'])
+  waittime = check((request.form['time']))
+  price = check((request.form['price']))
+  hour = check((request.form['hour']))
+
+  print(hour)
+  print(time.strftime("%H"))
+  print (cuisine)
+  print (waittime)
+  print (price)
+
+  if cuisine == None or waittime == None or price == None or hour == None:
+    context = "/search"
+    return render_template("error.html", url = context)
+
+  waittime = int(waittime)
+  price = int(price)
+  hour = float(hour) / 60
+
+  lowbound = price - 10
+  if price == 30:
+    price = sys.maxsize
+    lowbound = 20
+  
+  print(lowbound)
+  print(price)
+  hour = 15
+  # hour = hour + float(time.strftime("%H")) + float(time.strftime("%M")) / 60
+  
+  
+
+  info = []
+  cursor = g.conn.execute(""" SELECT name FROM restaurant a WHERE a.restaurantid IN (SELECT restaurantid FROM restaurantcategory b WHERE b.cuisince = %s AND b.waittime <= %s AND b.price >= %s AND b.price < %s) AND a.closing <= %s AND a.opening > %s;""", cuisine, waittime, lowbound, price, hour, hour)
+  for result in cursor:
+    info.append(result['name'])
+  cursor.close()
+
+  context = dict(info = info)
+
+  return render_template("suggest.html", **context)
 
 
 if __name__ == "__main__":
